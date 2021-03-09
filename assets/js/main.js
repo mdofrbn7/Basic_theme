659ac9c09ccd1a5f32ae3ed92e28bec5e4411e3e
rbnhoood7 <rbnhoood7@gmail.com>
2021-03-09 14:35:15 +0600

    header nav button fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-let toggle_button = document.querySelector(".nav .toggle-button");
-let header = documner.querySelector('header.header-area');
-toggleButton.onclink = function(e) {
-    header.classlist.toggle('nav-collapse');
+let toggleButton = document.querySelector("nav .toggle-button");
+let header = document.querySelector('header.header-area');
+toggleButton.onclick = function(e){
+    header.classList.toggle('nav-collapse');
 }
